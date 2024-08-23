6f6fe20a4d6e0b7011ab2760c771204a458b2b7b
rndexe <himanshu.ernde@gmail.com>
2024-08-23 19:00:14 +0530

    update github pages workflow

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-    return {
-        plugins: [react()],
-        base: mode === "development" ? "/" : "/space-explorer/",
-    };
+export default defineConfig({
+    base: process.env.BASE_PATH ?? '',
+    plugins: [react()],
 });
